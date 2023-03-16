e06f8b5f667879f0e118388896296ee25e5d14f5
Líria Li4 <lirialia51@gmail.com>
2023-03-16 16:37:50 +0100

    testando tamanhos

--- src/App.js
+++ src/App.js
@@ -23,16 +23,26 @@ function App() {
             fontSize="6xl"
             fontWeight="extrabold"
           >Olá Chakra</Text >
+
+          <Box w="10%" h="32px" bg="#fedadc" />
+
           <Text fontSize="md" >fontSize="md"</Text >
+
+          <Box width="10%" height={32} bg="#000" />
 
 
           <Text fontSize={32} >font-size 32px</Text>
 
+          <Box boxSize="sm" bg="#033300"/>
+
           <Text fontSize='2em' >fontSize='2em'</Text>
 
+          <Box w={256} bg="#abcffd"/>
+
           <Text textAlign={['left', 'center']} >textAlign={['left', 'center']}</Text>
-
-
+          <Box w="10%" h="32px" bg="#fde643"/>
+          <Text fontSize='2em' >--------------</Text>
+          <Box w='40px' bg="#244fff"/>
         </Box>
       </Box>
     </ChakraProvider>
